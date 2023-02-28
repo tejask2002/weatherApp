b3995a8a20cef55cf043e4b31e7a28dec7e47b6a
tejas katkade <tejas2002rk@gmail.com>
2023-02-28 23:31:17 +0530

    changes

--- public/js/main.js
+++ public/js/main.js
@@ -20,7 +20,7 @@ let myfun = async(e)=>{
   { 
     try{
       
-    let url=`https://api.openweathermap.org/data/2.5/weather?q=${cityval}&units=metric&appid=e850b03175e366bef355c385c2ee564c`;
+    let url=`https://api.openweathermap.org/data/2.5/weather?q=${cityval}&units=metric&appid=`;
     const response= await fetch(url);
     const data = await response.json();
     
